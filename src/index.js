148a6b1b15b786fafa43c43caff80322d78a69a5
Timowest12 <timowester@gmail.com>
2021-10-27 03:01:08 +0200

    Removed commented out code

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
-/* //import _ from 'lodash';
-import printMe from './print.js';
-import './style.css'; */
+
 import './style.css';
 
 const listoutput = document.querySelector('.todolist');
@@ -12,20 +10,3 @@ const outputlist = listobj.forEach((task) => {
   listoutput.innerHTML += `<li class='lstitem'><input type='checkbox' value='${task.index}'> ${task.description}</li>`;
 });
 outputlist();
-/*  function component() {
-
-   const element = document.createElement('div');
-  const btn = document.createElement('button');
-
-   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-   element.classList.add('hello');
-
-  btn.innerHTML = 'Click me and check the console!';
-  btn.onclick = printMe;
-
-  element.appendChild(btn);
-
-   return element;
- }
-
- document.body.appendChild(component()); */
